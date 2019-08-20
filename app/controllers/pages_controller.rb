8bbdf36567a1c670cc4bb7adebdcefe5b131d6cb
Tyler Gonsalves <tylergonsalves@Tylers-MacBook-Pro-2.local>
2019-08-20 16:46:52 +0200

    Create rails application and setup home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+    def alphablog
+    
+    end
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogs/new'
+  get 'dogs/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+  get 'pages/alphablog', to: 'pages#alphablog'  
+
+
+end
